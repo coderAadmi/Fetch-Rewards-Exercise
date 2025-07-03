dca887c3566226b5c1d3d9b147ea7959e7b4d62b
coderAadmi <pradyumnupadhyay2@gmail.com>
2025-07-03 12:50:29 -0700

    Fixed Api call sync issue, added refresh data, connectivity stattus, apiFailureResult

--- app/src/main/java/com/fetch/rewards/exercise/ExerciseAppModule.kt
+++ app/src/main/java/com/fetch/rewards/exercise/ExerciseAppModule.kt
@@ -2,6 +2,10 @@ package com.fetch.rewards.exercise
 
 import android.content.Context
 import androidx.room.Room
+import com.fetch.rewards.exercise.db.ExerciseAppDb
+import com.fetch.rewards.exercise.db.ListItemDao
+import com.fetch.rewards.exercise.network.ConnectivityService
+import com.fetch.rewards.exercise.network.ListApi
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -11,7 +15,6 @@ import dagger.hilt.components.SingletonComponent
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.create
 import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
 import kotlin.jvm.java
@@ -32,7 +35,10 @@ object ExerciseAppModule {
 
     @Singleton
     @Provides
-    fun provideListItemRepository(api : ListApi ,listItemDao: ListItemDao): ListItemRepository = ListItemRepository(api,listItemDao)
+    fun provideListItemRepository(api : ListApi, listItemDao: ListItemDao,
+                                  connectivityService: ConnectivityService
+    ): ListItemRepository = ListItemRepository(api,
+        listItemDao,connectivityService)
 
 
 
@@ -46,5 +52,11 @@ object ExerciseAppModule {
                 .writeTimeout(10000L, TimeUnit.MILLISECONDS).build())
             .addConverterFactory(GsonConverterFactory.create())
             .build().create(ListApi::class.java)
+
+
+    @Provides
+    fun provideConnectivityService(@ApplicationContext context: Context) : ConnectivityService =
+        ConnectivityService(context)
+
 
 }
